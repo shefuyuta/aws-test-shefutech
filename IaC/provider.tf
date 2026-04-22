66fd1c175297d5a93c0493ba7194aef30bf46edb
shefuyuta <77376816+shefuyuta@users.noreply.github.com>
2026-04-22 19:09:25 +0900

    Configure Terraform backend with S3 and DynamoDB

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "yuta-tf-state-xxxx"   # 作成したS3
+    key            = "env/dev/terraform.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "terraform-lock-table" # 作成したDynamoDB
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
