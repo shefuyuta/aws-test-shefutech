009fd127f8df9fd56a453aa3143ca9b6fc673788
shefuyuta <shefuyuta@gmail.com>
2026-04-23 20:57:00 +0900

    fixed eks.tf

--- IaC/eks.tf
+++ IaC/eks.tf
@@ -197,26 +197,24 @@ resource "helm_release" "alb_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
 
-  set {
-    name  = "clusterName"
-    value = aws_eks_cluster.cluster.name
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_controller.arn
-  }
-
+  set [
+    {
+      name  = "clusterName"
+      value = aws_eks_cluster.cluster.name
+    }
+    ,{
+      name  = "serviceAccount.create"
+      value = "true"
+    }
+    ,{
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    ,{
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.alb_controller.arn
+    }
+  ]
   depends_on = [
     aws_eks_node_group.nodes
   ]
